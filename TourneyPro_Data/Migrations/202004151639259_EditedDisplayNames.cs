a55c2f9cca40fc6bf2f711b466d248106e89a206
Joseph Hallam <joehallam13@gmail.com>
2020-04-15 13:25:08 -0400

    added migration

---
+++ TourneyPro_Data/Migrations/202004151639259_EditedDisplayNames.cs
@@ -0,0 +1,16 @@
+﻿namespace TourneyPro_Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EditedDisplayNames : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
